e60af28797e47bb9d7cf18612bd2198e1ae513d3
Zaur <huszaur010@gmail.com>
2025-01-24 10:20:09 -0500

    Amazon sign in

---
+++ amazon_sign_in.py
@@ -0,0 +1,51 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from time import sleep
+
+# get the path to the ChromeDriver executable
+driver_path = ChromeDriverManager().install()
+
+# create a new Chrome browser instance
+service = Service(driver_path)
+driver = webdriver.Chrome(service=service)
+driver.maximize_window()
+driver.get("https://www.amazon.com/")
+
+#Sign in page
+#driver.find_element(By.ID,'nav-link-accountList-nav-line-1').click()
+
+#Amazon logo
+#driver.find_element(By.XPATH,"//i[@aria-label='Amazon']").click()
+
+#Email field
+#driver.find_element(By.ID,"ap_email").send_keys("zaur@gmail.com")
+
+#Continue button
+#driver.find_element(By.ID,"continue").click()
+
+#Conditions of use link
+#driver.find_element(By.XPATH,"//a[contains(@href, 'ap_signin_notification_condition_of_use')]").click()
+
+#Privacy Notice link
+#driver.find_element(By.XPATH,"//a[contains(@href, 'ap_signin_notification_privacy_notice')]").click()
+
+#Need help link
+#driver.find_element(By.XPATH,"//a[@data-action='a-expander-toggle']").click()
+
+#Forgot your password link
+#driver.find_element(By.ID,"auth-fpp-link-bottom").click()
+
+#More ways to get support Link
+#driver.find_element(By.ID,"ap-other-signin-issues-link").click()
+
+#Shop on Amazon Business Link
+#driver.find_element(By.ID,"ab-signin-ingress-link").click()
+
+#Create your Amazon account button
+#driver.find_element(By.ID,"createAccountSubmit").click()
+
+sleep(5)
+
+
